d5cb0971d0d8da1e02321ef598746d1994f29262
Ben Shope <nimajnebs@gmail.com>
2013-06-03 20:09:21 -0700

    Add companies json file

--- app/js/app.js
+++ app/js/app.js
@@ -90,28 +90,3 @@ app.directive('pieChart', function ($timeout) {
     }
   };
 });
-
-app.controller('Ctrl', function($scope) {
-  // Initial chart data
-  $scope.chartTitle = "Company Name";
-  $scope.chartWidth = 500;
-  $scope.chartHeight = 320;
-  $scope.chartData = [
-    ['Democrat',     60],
-    ['Republican',      40]
-  ];
-
-  $scope.deleteRow = function (index) {
-    $scope.chartData.splice(index, 1);
-  };
-  $scope.addRow = function () {
-    $scope.chartData.push([]);
-  };
-  $scope.selectRow = function (index) {
-    $scope.selected = index;
-  };
-  $scope.rowClass = function (index) {
-    return ($scope.selected === index) ? "selected" : "";
-  };
-});
-
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,29 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+function Ctrl($scope) {
+    $scope.chartTitle = "Company Name";
+    $scope.chartWidth = 500;
+    $scope.chartHeight = 320;
+    $scope.chartData = [
+    ['Democrat',     60],
+    ['Republican',      40]
+    ];
 
+    $scope.deleteRow = function (index) {
+    $scope.chartData.splice(index, 1);
+    };
+    $scope.addRow = function () {
+    $scope.chartData.push([]);
+    };
+    $scope.selectRow = function (index) {
+    $scope.selected = index;
+    };
+    $scope.rowClass = function (index) {
+    return ($scope.selected === index) ? "selected" : "";
+    };
 
-function MyCtrl2() {
+    $scope.companies = $http.get('companies/companies.json').success(function(data) {
+    $scope.phones = data;
+  });;
 }
-MyCtrl2.$inject = [];
